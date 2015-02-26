becd363b6eb41d36fe4e9128d65592e1ff56f049
Steven Silvester <steven.silvester@ieee.org>
2015-02-25 20:15:53 -0600

    Make install-able

---
+++ setup.py
@@ -0,0 +1,50 @@
+"""Setup script for image_inspector package.
+"""
+DISTNAME = 'image_inspector'
+DESCRIPTION = 'Image Interaction widgets and viewer.'
+LONG_DESCRIPTION = open('README.rst', 'rb').read().decode('utf-8')
+MAINTAINER = 'Steven Silvester'
+MAINTAINER_EMAIL = 'steven.silvester@ieee.org'
+URL = 'http://github.com/blink1073/image_inspector'
+LICENSE = 'MIT'
+REQUIRES = ["numpy (>= 1.7.1)", "matplotlib (>= 1.4)"]
+CLASSIFIERS = """\
+Intended Audience :: Developers
+Intended Audience :: Science/Research
+License :: OSI Approved :: MIT License
+Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3.3
+Programming Language :: Python :: 3.4
+Topic :: Scientific/Engineering
+Topic :: Software Development
+"""
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('image_inspector/__init__.py', 'rb') as fid:
+    for line in fid:
+        line = line.decode('utf-8')
+        if line.startswith('__version__'):
+            version = line.strip().split()[-1][1:-1]
+            break
+
+
+setup(
+    name=DISTNAME,
+    version=version,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
+    url=URL,
+    download_url=URL,
+    license=LICENSE,
+    platforms=["Any"],
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    classifiers=list(filter(None, CLASSIFIERS.split('\n'))),
+    requires=REQUIRES
+ )
